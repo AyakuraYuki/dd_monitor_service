4fe8520adfe2052b968aae4363ff826ab4b5d60f
Ayakura Yuki <AyakuraYuki@users.noreply.github.com>
2019-08-29 17:34:14 +0800

    gh-pages

--- ui/src/api/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-export const BASE_API = 'http://127.0.0.1:5140/'
-const api = axios.create()
-api.defaults.timeout = 10000
-api.defaults.headers.post['Content-Type'] = 'application/json;charset=UTF-8'
-api.defaults.baseURL = BASE_API
-
-export default api
--- ui/src/api/player.js
+++
@@ -1,6 +0,0 @@
-import api from './api'
-
-export const playlist = () => {
-    const uri = '/player/list'
-    return api.get(uri)
-}
