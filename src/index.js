d39149d400ab3878b590d25b93f3b9fdd25328a1
Ayakura Yuki <AyakuraYuki@users.noreply.github.com>
2020-03-06 18:00:48 +0800

    feat: try to add logger

--- src/index.js
+++ src/index.js
@@ -1,13 +1,45 @@
 const { app, BrowserWindow } = require("electron")
-// const appConfigDir = app.getPath('userData')
-// const appCoreDownloadDir = `${appConfigDir}/dd_monitor_core`
+const fs = require('fs')
+const exec = require('child_process').exec
+const downloadGitRepo = require('download-git-repo')
+const log = require('electron-log')
+
+// constant
+const appPath = app.getPath('userData')
+const appCorePath = `${appPath}/core`
+const logFolder = `${appPath}/logs`
+
+// logger
+export const logPath = `${logFolder}/dd-monitor.log`
+log.transports.file.file = logPath
+log.transports.file.format = '{y}-{m}-{d} {h}:{i}:{s}:{ms} [{level}] {text}'
+log.transports.file.maxSize = 5 * 1024 * 1024
+log.transports.file.level = 'info'
+log.transports.console.format = '{y}-{m}-{d} {h}:{i}:{s}:{ms} [{level}] {text}'
+if (process.env.NODE_ENV === 'dev') {
+    log.transports.console.level = 'debug'
+} else {
+    log.transports.console.level = 'info'
+}
 
 let win
 
-function sleep(delay) {
-    const start = (new Date()).getTime();
-    while ((new Date()).getTime() - start < delay) {
-        // DO NOTHING
+function initCore() {
+    log['debug'](`appPath: ${appPath}`)
+    log['debug'](`appCorePath: ${appCorePath}`)
+
+    if (fs.existsSync(appCorePath)) {
+        let pathStat = fs.statSync(appCorePath)
+        if (!(pathStat.isDirectory())) {
+            fs.unlinkSync(appCorePath)
+            downloadGitRepo(`AyakuraYuki/dd_monitor#backend`, appCorePath, err => {
+                log[err ? 'error' : 'info'](`Core download ${err ? 'error' : 'success'}`)
+            })
+        }
+    } else {
+        downloadGitRepo(`AyakuraYuki/dd_monitor#backend`, appCorePath, err => {
+            log[err ? 'error' : 'info'](`Core download ${err ? 'error' : 'success'}`)
+        })
     }
 }
 
@@ -16,8 +48,6 @@ function createWindow() {
         width: 1600,
         height: 1080,
     })
-
-    sleep(100)
 
     win.loadFile('./dist/index.html')
         .then(() => {
@@ -31,7 +61,14 @@ function createWindow() {
     })
 }
 
-app.on("ready", createWindow)
+app.on("ready", () => {
+    initCore()
+    createWindow()
+})
+
+app.on("before-quit", () => {
+
+})
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
@@ -48,3 +85,10 @@ app.on('activate', () => {
 // app.on("quit", () =>
 //     exec("ps -ef | grep 'python run-dd-monitor.py' | grep -v grep | awk '{print $2}' | xargs kill")
 // )
+
+// function sleep(delay) {
+//     const start = (new Date()).getTime();
+//     while ((new Date()).getTime() - start < delay) {
+//         // DO NOTHING
+//     }
+// }
