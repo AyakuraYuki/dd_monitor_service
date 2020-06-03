bee14068fcb55aebcf627ef24ea9ef2f759cd51f
Ayakura Yuki <AyakuraYuki@users.noreply.github.com>
2020-06-03 10:40:49 +0800

    [feat] try to mix typescript into this project

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,26 @@ module.exports = {
     env: {
         node: true
     },
-    'extends': [
-        'plugin:vue/essential',
-        'eslint:recommended'
-    ],
+    // 'extends': [
+    //     'plugin:vue/essential',
+    //     'eslint:recommended'
+    // ],
     rules: {
         'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
     },
+    // parserOptions: {
+    //     parser: 'babel-eslint'
+    // }
+    plugins: [
+        '@typescript-eslint'
+    ],
+    extends: [
+        'plugin:vue/essential',
+        'eslint:recommended',
+        '@vue/typescript'
+    ],
     parserOptions: {
-        parser: 'babel-eslint'
+        parser: '@typescript-eslint/parser'
     }
 }
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+let WebpackChain = import('webpack-chain')
+
 module.exports = {
     publicPath: './',
     // build output dir
@@ -7,5 +9,36 @@ module.exports = {
     // Disable source map.
     productionSourceMap: false,
     // Disable filename hash
-    filenameHashing: true
+    filenameHashing: true,
+
+    /**
+     * Typescript in Webpack
+     *
+     * @param {WebpackChain} config
+     */
+    chainWebpack: (config) => {
+        config
+            .resolve
+            .extensions
+            .add('.ts')
+            .add('.tsx')
+            .end()
+            .end()
+            .module
+            .rule('typescript')
+            .test(/\.tsx?$/)
+            .use('babel-loader')
+            .loader('babel-loader')
+            .end()
+            .use('ts-loader')
+            .loader('ts-loader')
+            .options({
+                transpileOnly: true,
+                appendTsSuffixTo: [
+                    '\\.vue$',
+                ],
+                happyPackMode: false
+            })
+            .end()
+    }
 }
