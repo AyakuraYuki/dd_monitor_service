b94551ec56eaf8925b246781af19088399e2feb4
Ayakura Yuki <AyakuraYuki@users.noreply.github.com>
2020-07-09 11:54:14 +0800

    [fix] add `&autoplay=1` to the end of an embed url

--- src/components/monitor/YouTubePlayer.vue
+++ src/components/monitor/YouTubePlayer.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="youtube-player" class="col embed-responsive embed-responsive-16by9">
         <iframe class="embed-responsive-item"
-                :src="src"
+                :src="standardize(src)"
                 allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
                 allowfullscreen></iframe>
     </div>
@@ -12,6 +12,15 @@
         name: "YoutubePlayer",
         props: {
             src: String
+        },
+        methods: {
+            standardize (src) {
+                if (src) {
+                    if (src.indexOf("&autoplay=1") < 0) {
+                        return `${src}&autoplay=1`
+                    }
+                }
+            }
         }
     }
 </script>
